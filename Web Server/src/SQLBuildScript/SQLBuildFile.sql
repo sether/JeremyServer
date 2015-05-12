e75c4760eada63ab475581a2df87a961659e3268
Gavinhad <rymika96@gmail.com>
2015-05-12 14:24:11 +1000

    BuildFile Update

--- Web Server/src/SQLBuildScript/SQLBuildFile.sql
+++ Web Server/src/SQLBuildScript/SQLBuildFile.sql
@@ -1,19 +1,33 @@
-CREATE DATABASE UserDatabase;
-USE UserDatabase
+CREATE DATABASE JeremyAPIDatabase;
+USE JeremyAPIDatabase
 
 CREATE TABLE User(
 email VARCHAR(30), 
 firstName VARCHAR(20), 
 lastName VARCHAR(20), 
 password VARCHAR(20), 
-CreditCardNumber VARCHAR(20), 
-APIKey VARCHAR(20), 
+creditCardNumber VARCHAR(20), 
+apiKey VARCHAR(256), 
 PRIMARY KEY (email));
+
+
+CREATE TABLE APIMethod(
+methodType ENUM ('html', 'rest'), 
+price ('0.10', '0.20',)
+PRIMARY KEY (methodType));
+
 
 CREATE TABLE APIEvents(
 id INT NOT NULL AUTO_INCREMENT,
-APIKey VARCHAR(20),
-LogDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-Description VARCHAR(20),
-PRIMARY KEY (id));
+email VARCHAR(30),
+apiKey VARCHAR(256),
+logDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+fileName VARCHAR(50),
+size VARCHAR(20),
+converionType ENUM ('html', 'rest'),
+methodType ENUM ('html', 'rest'),
+description VARCHAR(500),
+PRIMARY KEY (id),
+FOREIGN KEY (email) REFERENCES User(email),
+FOREIGN KEY (methodType) REFERENCES APIMethod(methodType));
 ;
