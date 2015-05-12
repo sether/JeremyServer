bba0e982b75f0b03c59d0acab07904fb8dfd39d5
Scott Micklethwaite <scott.micklethwaite@hotmail.com>
2015-05-12 11:23:40 +1000

    index servlet

--- Web Server/WebContent/nav.jsp
+++ Web Server/WebContent/nav.jsp
@@ -2,10 +2,8 @@
 	<ul>
 		<li class="active"><a href="index.jsp">Home</a>
 		<li><a href="converter">Converter</a>
-		<li><a href="register">Register</a>
-		<li><a href="login.jsp">Login</a>
 		<li><a href="contactUs.html">Contact Us</a>
 		<li><a href="documentation.html">Documentation</a>
-		<li>${loginLink}
+		${loginLink}
 	</ul>
 </nav>
---
+++ Web Server/src/WebServiceJeremy/IndexServlet.java
@@ -0,0 +1,54 @@
+package WebServiceJeremy;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class IndexServlet extends HttpServlet {
+    // Processes requests for both HTTP <code>GET</code> and <code>POST</code>.
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    	HttpSession session = request.getSession();
+    	String content = "";
+    	
+    	getIndexPage();
+        
+        RequestDispatcher view = request.getRequestDispatcher("template.jsp"); // use this view
+        request.setAttribute("content", content); //set this value - the page will display the value in the content section
+        request.setAttribute("loginLink", LoginStatusTemp.displayLoginWidget(session));
+        view.forward(request, response);
+    }
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /** 
+    * Handles the HTTP <code>GET</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+    * Handles the HTTP <code>POST</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+       processRequest(request, response);
+    }
+    
+    public String getIndexPage(){
+    	return  "";
+    }
+}
