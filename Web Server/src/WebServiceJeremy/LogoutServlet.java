7f6270182723208d8e853f15b339034c87e2734a
Scott Micklethwaite <scott.micklethwaite@hotmail.com>
2015-05-14 10:16:34 +1000

    extended servlet

--- Web Server/src/WebServiceJeremy/LogoutServlet.java
+++ Web Server/src/WebServiceJeremy/LogoutServlet.java
@@ -3,11 +3,12 @@ package WebServiceJeremy;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LogoutServlet {
+public class LogoutServlet extends HttpServlet {
 
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 		    throws ServletException, IOException {
