b85b66c714a7d4f1313e200fe4ba3c3e4d324643
Scott Micklethwaite <scott.micklethwaite@hotmail.com>
2015-05-14 10:03:08 +1000

    added logout servlet

---
+++ Web Server/src/WebServiceJeremy/LogoutServlet.java
@@ -0,0 +1,38 @@
+package WebServiceJeremy;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet {
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+		    throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("status", null);
+		response.sendRedirect("index");
+	}
+	
+	/** 
+    * Handles the HTTP <code>GET</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    } 
+
+    /** 
+    * Handles the HTTP <code>POST</code> method.
+    * @param request servlet request
+    * @param response servlet response
+    */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
