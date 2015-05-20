5c2002aee07a84a423c64dfa8f7d8605542303a2
Gavinhad <rymika96@gmail.com>
2015-05-21 09:57:21 +1000

    build File

--- Web Server/src/SQLBuildScript/SQLBuildFile.sql
+++ Web Server/src/SQLBuildScript/SQLBuildFile.sql
@@ -2,10 +2,11 @@ CREATE DATABASE JeremyAPIDatabase;
 USE JeremyAPIDatabase;
 
 CREATE TABLE User(
-email VARCHAR(50), 
-firstName VARCHAR(20), 
-lastName VARCHAR(20), 
-password VARCHAR(20), 
+email VARCHAR(70), 
+firstName VARCHAR(30), 
+lastName VARCHAR(30), 
+password VARCHAR(30), 
+salt VARCHAR(30),
 creditCardNumber VARCHAR(20), 
 publicApiKey VARCHAR(1024), 
 privateApiKey VARCHAR(1024),
@@ -13,12 +14,12 @@ PRIMARY KEY (email));
 
 CREATE TABLE APIEvents(
 id INT NOT NULL AUTO_INCREMENT,
-email VARCHAR(50),
+email VARCHAR(70),
 publicApiKey VARCHAR(1024),
 logDate DATETIME DEFAULT CURRENT_TIMESTAMP,
 fileName VARCHAR(50),
-size VARCHAR(20),
+size VARCHAR(30),
 methodType ENUM ('HTML', 'REST'),
-description VARCHAR(500),
+description VARCHAR(1000),
 PRIMARY KEY (id),
 FOREIGN KEY (email) REFERENCES User(email));
